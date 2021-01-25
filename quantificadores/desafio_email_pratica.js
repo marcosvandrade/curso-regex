1781d3990174c2d7099ccbaeb301e80c7e3ee584
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2021-01-25 09:43:47 -0300

    work25jan21

--- quantificadores/desafio_email_pratica.js
+++ quantificadores/desafio_email_pratica.js
@@ -2,4 +2,4 @@ const texto = `Os e-mails dos convidados são:
 - fulano@cod3r.com.br
 - xico@gmail.com ...`
 
-console.log(texto.match(/@.*\b/g))
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}.?\w{0,2}/g))
